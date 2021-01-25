c3dd9ebd98cbc82434c89c38381c7c2c8e48a906
Jamie Garrett <garrettjamie87@googlemail.com>
2021-01-25 17:17:08 +0100

    for loop and event listener to modal in shared.js

--- shared.js
+++ shared.js
@@ -1,1 +1,15 @@
-alert('this works')
+// alert('this works')
+
+let backdrop = document.querySelector(".backdrop");
+let modal = document.querySelectorAll(".modal");
+// console.dir(backdrop);
+
+let selectPlanButton = document.querySelectorAll(".plan button");
+for (let i = 0; i < selectPlanButton.length; i++) {
+  selectPlanButton[i].addEventListener("click", function () {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
+}
+
+// console.dir(selectPlanButton)
