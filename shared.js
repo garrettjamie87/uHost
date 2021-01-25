001c4a8ba8886a342a14acf6a66f9d7f360b8c01
Jamie Garrett <garrettjamie87@googlemail.com>
2021-01-25 18:20:47 +0100

    add hamburger menu and side navigation

--- shared.js
+++ shared.js
@@ -1,13 +1,37 @@
-// alert('this works')
-
 let backdrop = document.querySelector(".backdrop");
 let modal = document.querySelector(".modal");
+let modalNoButton = document.querySelector(".modal__action--negative");
 let selectPlanButtons = document.querySelectorAll(".plan button");
+let toggleButton = document.querySelector(".toggle-button");
+let mobileNav = document.querySelector(".mobile-nav");
 
 // console.dir(backdrop);
 for (let i = 0; i < selectPlanButtons.length; i++) {
-      selectPlanButtons[i].addEventListener("click", function() {
-        modal.style.display = "block";
-        backdrop.style.display = "block";
-      });
-    }
+  selectPlanButtons[i].addEventListener("click", function () {
+    //     modal.style.display = "block";
+    //     backdrop.style.display = "block";
+    modal.classList.add("open");
+    backdrop.classList.add("open");
+  });
+}
+
+backdrop.addEventListener("click", function () {
+//   mobileNav.style.display = "none";
+mobileNav.classList.remove('open');
+  closeModal();
+});
+
+modalNoButton.addEventListener("click", closeModal);
+
+function closeModal() {
+  //   backdrop.style.display = "none";
+  //   modal.style.display = "none";
+  modal.classList.remove("open");
+  backdrop.classList.remove("open");
+}
+
+toggleButton.addEventListener("click", function () {
+//  
+mobileNav.classList.add('open');
+backdrop,classList.add('open');
+});
